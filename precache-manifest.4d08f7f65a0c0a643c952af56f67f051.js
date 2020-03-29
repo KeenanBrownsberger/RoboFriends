56406b2bfcef18c34b4688bf262eae7833536b9d
keenanxcak <kbrownsberger16@my.whitworth.edu>
2020-03-29 16:32:25 -0700

    Updates

---
+++ precache-manifest.4d08f7f65a0c0a643c952af56f67f051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840f5c0704cbee7dd1c3d6351f0da164",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "92323847c64b0725f42d",
+    "url": "/RoboFriends/static/css/main.37b447b2.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoboFriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92323847c64b0725f42d",
+    "url": "/RoboFriends/static/js/main.9c360759.chunk.js"
+  },
+  {
+    "revision": "3b5acba4a49444d74d44",
+    "url": "/RoboFriends/static/js/runtime-main.f1304c14.js"
+  }
+]);
